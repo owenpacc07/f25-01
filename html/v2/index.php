731359bfdbdd2639c3d7018234f4a5ae28dd8449
gavinbell123 <157721855+gavinbell123@users.noreply.github.com>
2025-09-20 01:23:52 -0700

    Added names

--- html/v2/index.php
+++ html/v2/index.php
@@ -381,7 +381,7 @@ if ($coremode == 'core-a') {
       <footer class="bg-light text-center text-lg-start">
 
         <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
-	<p>Fall 2025 Contributors: Jack Lin | Michael Scotto | Owen Pacchiana | Gavin Bell </p>
+	  <p>Fall 2025 Contributors: Gavin Bell | Jack Lin | Michael Scotto | Owen Pacchiana </p>
         <p>Spring 2025 Contributors: Justin Feinman | Henry Becker </p>
 	  <p>Fall 2024 Contributors: William Rubin | Marco Lemus </p>
 	  <p>Spring 2024 Contributors: Shelby Hinton | Gianella Robles | Mary Seelmann </p>
